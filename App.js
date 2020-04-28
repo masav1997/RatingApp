553fa978e4809751f66bdd077aef4965f8e338f8
Anna <anna@iMac-Alexey.local>
2020-04-28 15:14:08 +0300

    add safeAreaView to navigator

--- App.js
+++ App.js
@@ -28,6 +28,7 @@ import ReviewCompany from './screens/ReviewCompany';
 import CheckList from './screens/CheckList';
 import Map from './screens/Map';
 import SectionListModule from './components/ContactList/SectionListContacts/';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -103,92 +104,102 @@ export default class App extends React.Component {
 			);
 		}
 		return (
-			<NavigationContainer style={{ backgroundColor: '#5227D0' }}>
-				<StatusBar barStyle="light-content" />
-				<Stack.Navigator
-					screenOptions={{
-						headerShown: false,
-					}}
-				>
-					<Stack.Screen name="SignInShop" component={SignInShop} />
-					<Stack.Screen name="SignUpShop" component={SignUpShop} />
-					<Stack.Screen name="SignUpUser" component={SignUpUser} />
-					<Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-					<Stack.Screen name="CreatePassword" component={CreatePassword} />
-					<Stack.Screen name="MainPage" component={MainPage} />
-					<Stack.Screen name="About" component={About} />
-					<Stack.Screen name="Code" component={Code} />
-					<Stack.Screen name="Check" component={Check} />
-					<Stack.Screen name="Profile1" component={Profile1} />
-					<Stack.Screen name="ProfileCompany" component={ProfileCompany} />
-					<Stack.Screen name="ProfileCompany1" component={ProfileCompany1} />
-					<Stack.Screen name="MyCompany" component={MyCompany} />
-					<Stack.Screen name="Filters" component={Filters} />
-					<Stack.Screen name="Messages" component={Messages} />
-					<Stack.Screen name="Contacts" component={Contacts} />
-					<Stack.Screen name="ReviewVoice" component={ReviewVoice} />
-					<Stack.Screen name="Search" component={Search} />
-					<Stack.Screen name="ReviewCompany" component={ReviewCompany} />
-					<Stack.Screen name="Map" component={Map} />
-					<Stack.Screen name="SectionItem" component={SectionListModule} />
-					<Stack.Screen name="CheckList">
-						{() => (
-							<Tab.Navigator
-								screenOptions={({ route }) => ({
-									tabBarIcon: ({ focused }) => {
-										let iconName;
-										let style;
+			<SafeAreaProvider>
+				<NavigationContainer style={{ backgroundColor: '#5227D0' }}>
+					<StatusBar barStyle="light-content" />
+					<Stack.Navigator
+						screenOptions={{
+							headerShown: false,
+						}}
+					>
+						<Stack.Screen name="SignInShop" component={SignInShop} />
+						<Stack.Screen name="SignUpShop" component={SignUpShop} />
+						<Stack.Screen name="SignUpUser" component={SignUpUser} />
+						<Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+						<Stack.Screen name="CreatePassword" component={CreatePassword} />
+						<Stack.Screen name="MainPage" component={MainPage} />
+						<Stack.Screen name="About" component={About} />
+						<Stack.Screen name="Code" component={Code} />
+						<Stack.Screen name="Check" component={Check} />
+						<Stack.Screen name="Profile1" component={Profile1} />
+						<Stack.Screen name="ProfileCompany" component={ProfileCompany} />
+						<Stack.Screen name="ProfileCompany1" component={ProfileCompany1} />
+						<Stack.Screen name="MyCompany" component={MyCompany} />
+						<Stack.Screen name="Filters" component={Filters} />
+						<Stack.Screen name="Messages" component={Messages} />
+						<Stack.Screen name="Contacts" component={Contacts} />
+						<Stack.Screen name="ReviewVoice" component={ReviewVoice} />
+						<Stack.Screen name="Search" component={Search} />
+						<Stack.Screen name="ReviewCompany" component={ReviewCompany} />
+						<Stack.Screen name="Map" component={Map} />
+						<Stack.Screen name="SectionItem" component={SectionListModule} />
+						<Stack.Screen name="CheckList">
+							{() => (
+								<Tab.Navigator
+									screenOptions={({ route }) => ({
+										tabBarIcon: ({ focused }) => {
+											let iconName;
+											let style;
 
-										if (route.name === 'Scan') {
-											iconName = focused
-												? require('./assets/icons/qr.png')
-												: require('./assets/icons/qr.png');
-											style = { width: 20, height: 20, bottom: 10, tintColor: '#B5BBC9' };
-										} else if (route.name === 'CheckList') {
-											iconName = focused
-												? require('./assets/icons/checks.png')
-												: require('./assets/icons/checks.png');
-											style = { width: 23, height: 20, bottom: 10, tintColor: '#B5BBC9' };
-										} else if (route.name === 'EditProfile') {
-											iconName = focused
-												? require('./assets/icons/note.png')
-												: require('./assets/icons/note.png');
-											style = { width: 15, height: 24, bottom: 10, tintColor: '#B5BBC9' };
-										} else if (route.name === 'Profile') {
-											iconName = focused
-												? require('./assets/icons/profile1.png')
-												: require('./assets/icons/profile1.png');
-											style = { width: 19, height: 20, bottom: 10, tintColor: '#B5BBC9' };
-										}
+											if (route.name === 'Scan') {
+												iconName = focused
+													? require('./assets/icons/qr.png')
+													: require('./assets/icons/qr.png');
+												style = focused
+													? { width: 20, height: 20, bottom: 10, tintColor: '#5227D0' }
+													: { width: 20, height: 20, bottom: 10, tintColor: '#B5BBC9' };
+											} else if (route.name === 'CheckList') {
+												iconName = focused
+													? require('./assets/icons/checks.png')
+													: require('./assets/icons/checks.png');
+												style = focused
+													? { width: 23, height: 20, bottom: 10, tintColor: '#5227D0' }
+													: { width: 23, height: 20, bottom: 10, tintColor: '#B5BBC9' };
+											} else if (route.name === 'EditProfile') {
+												iconName = focused
+													? require('./assets/icons/note.png')
+													: require('./assets/icons/note.png');
+												style = focused
+													? { width: 15, height: 24, bottom: 10, tintColor: '#5227D0' }
+													: { width: 15, height: 24, bottom: 10, tintColor: '#B5BBC9' };
+											} else if (route.name === 'Profile') {
+												iconName = focused
+													? require('./assets/icons/profile1.png')
+													: require('./assets/icons/profile1.png');
+												style = focused
+													? { width: 19, height: 20, bottom: 10, tintColor: '#5227D0' }
+													: { width: 19, height: 20, bottom: 10, tintColor: '#B5BBC9' };
+											}
 
-										return <Image source={iconName} style={style} />;
-									},
-								})}
-								tabBarOptions={{
-									style: {
-										borderTopLeftRadius: 20,
-										borderTopRightRadius: 20,
-										backgroundColor: '#FFFFFF',
-										position: 'absolute',
-										bottom: -1,
-										padding: 15,
-										height: 54,
-										zIndex: 8,
-										borderTopColor: '#FFFFFF',
-										borderTopWidth: 0.3,
-									},
-									showLabel: false,
-								}}
-							>
-								<Tab.Screen name="Scan" component={Scan} />
-								<Tab.Screen name="CheckList" component={CheckList} />
-								<Tab.Screen name="EditProfile" component={EditProfile} />
-								<Tab.Screen name="Profile" component={Profile} />
-							</Tab.Navigator>
-						)}
-					</Stack.Screen>
-				</Stack.Navigator>
-			</NavigationContainer>
+											return <Image source={iconName} style={style} />;
+										},
+									})}
+									tabBarOptions={{
+										style: {
+											borderTopLeftRadius: 20,
+											borderTopRightRadius: 20,
+											backgroundColor: '#FFFFFF',
+											position: 'absolute',
+											bottom: -1,
+											padding: 15,
+											height: 54,
+											zIndex: 8,
+											borderTopColor: '#FFFFFF',
+											borderTopWidth: 0.3,
+										},
+										showLabel: false,
+									}}
+								>
+									<Tab.Screen name="Scan" component={Scan} />
+									<Tab.Screen name="CheckList" component={CheckList} />
+									<Tab.Screen name="EditProfile" component={EditProfile} />
+									<Tab.Screen name="Profile" component={Profile} />
+								</Tab.Navigator>
+							)}
+						</Stack.Screen>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</SafeAreaProvider>
 		);
 	}
 }
